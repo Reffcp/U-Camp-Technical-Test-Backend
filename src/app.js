5eb83890534336abbbbf9ca003c054a255492ef0
reffcp <yo@cristiancovarrubias.online>
2022-06-16 20:01:56 -0500

    se crea el servidor inicial con express

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// se carga el archivo de configuracion de variables de entorno
+dotenv.config();
+
+// se crea el servidor, utilizando json como formato de datos y cors para permitir peticiones desde cualquier origen de momento
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// se corre el servidor
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+}
+);
+
+// se exporta el servidor para que sea utilizado en otros archivos
+module.exports = app;
